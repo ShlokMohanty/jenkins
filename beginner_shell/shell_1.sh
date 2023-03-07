bebba46f34f2d2c01b29078bbfdc7208c99e23a0
Shlok Mohanty <59838234+ShlokMohanty@users.noreply.github.com>
2023-03-07 12:39:52 +0530

    Create shell_1.sh

---
+++ beginner_shell/shell_1.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+a=10
+b=9
+#test $a -lt $b && false              if a  is less than b
+#test $a -gt $b && false              if a is greater than b
+echo "111"
+
+#if a is greater than b 
+#!/bin/bash
+set -e
+a=10
+b=9
+test $a -le $b
+echo "111"
+
+
+
